6f95a8095cefc584253859cd8d9dae7989801224
Federico Rota <federico.rota01@gmail.com>
2015-10-07 16:52:25 +0200

    First commit

---
+++ gruntfile.js
@@ -0,0 +1,36 @@
+﻿module.exports = function (grunt) {
+    'use strict';
+    // Project configuration.
+    grunt.initConfig({
+        
+        watch: {
+            myStyles: {
+                files: ['gruntfile.js', 'Content/app/**/*.scss','Content/style.scss'],
+                tasks: 'sass'
+            }
+        },
+        sass: {
+            dist: {
+                options: {                       // Target options
+                    sourcemap: 'auto'
+                },
+                files: [
+                {
+                    expand: true,
+                    cwd: 'Content/app',
+                    src: ['*.scss'],
+                    dest: 'Content/app',
+                    ext: '.css'
+                }, {
+                    'Content/style.css': 'Content/style.scss'
+                }]
+            }
+        }
+        
+    });
+
+    // Load the Grunt plugins.
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.registerTask('default', [ 'sass', 'watch']);
+};
